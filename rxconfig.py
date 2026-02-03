05c9ee0dfedc7b574eda530c8a7987b624ceaf3a
jacky.jl <116127513+jackysmith040@users.noreply.github.com>
2026-02-03 15:28:04 +0000

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="dashboard",
-    api_url="https://dashboard-re25.onrender.com",
+    api_url="https://dashboard-ygop.onrender.com",
     show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
