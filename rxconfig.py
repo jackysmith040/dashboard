d62d3f50181a1bb36801a9870eb7c3f6ef778210
jackysmith040 <jackysmith040@gmail.com>
2026-02-03 20:35:16 +0000

    Made the following commit: - Updated backend and frontend port numbers in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="dashboard",
+    backend_port=4000,
+    frontend_port=3000,
     show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
