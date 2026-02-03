aef4ae2163c2b6d22da371c199224b33bfcae272
jackysmith040 <jackysmith040@gmail.com>
2026-02-03 11:01:16 +0000

    add the backend url

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="dashboard",
+    api_url="https://dashboard-re25.onrender.com:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
