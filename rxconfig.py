b44504873dce9bc4213db202c849cc9686e152e4
jackysmith040 <jackysmith040@gmail.com>
2026-02-03 11:36:31 +0000

    fixed url and banner

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="dashboard",
-    api_url="https://dashboard-re25.onrender.com:8000",
+    api_url="https://dashboard-re25.onrender.com",
+    show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
