b9115adc5dd5a8f4e74decab7c980af250ea7dc3
jacky.jl <116127513+jackysmith040@users.noreply.github.com>
2026-02-03 15:42:33 +0000

    Update rxconfig.py remove api_url

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="dashboard",
-    api_url="https://dashboard-ygop.onrender.com",
     show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
